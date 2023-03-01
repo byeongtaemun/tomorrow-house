1dad61d436d245588d52e53ad1233864086902ea
byeongtaemun <munbyeongtae@gmail.com>
2023-03-01 23:31:17 +0900

    backup

---
+++ js/carousels.js
@@ -0,0 +1,33 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  controlsContainer: '.user-gallery-controls',
+  navContainer: '.user-gallery .thumbnail-list',
+  navAsThumbnails: true,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      edgePadding: 75,
+      controls: true,
+    },
+  },
+})
